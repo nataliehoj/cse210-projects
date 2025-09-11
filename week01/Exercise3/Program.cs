87c2a37c7d78142366c4db545642b8c484ccf80f
nataliehoj <nhojnacke@byupathway.edu>
2025-09-10 20:51:04 -0700

    exercise 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+                Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is the number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("The number is HIGHER than that");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("The number is LOWER than that");
+            }
+            else
+            {
+                Console.WriteLine("Correct! *cue Final Fantasy V victory theme yet again lol*");
+            }
+        }
     }
 }
